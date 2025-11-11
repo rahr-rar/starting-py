707532fcf8f2e7c6505e4f8d8a030e6b7c206c84
rahr-rar <israelcostaaj@gmail.com>
2025-11-11 20:27:09 -0300

    first commit

---
+++ hello.py
@@ -0,0 +1,2 @@
+print ('Hello world')
+print (7+10)
---
+++ teste1.py
@@ -0,0 +1,12 @@
+nome = input('Diite o seu nome: ')
+sobrenome = input('Digite o seu sobrenome: ')
+idade = int (input ('Digite a sua idade: '))
+ano_nascimento = int (input ('Digite o ano que você nasceu: '))
+maior_idade = (idade >= 18)
+altura_metros = float (input ('Digite qual é a sua altura: '))
+
+
+print('Usuario:', nome, 'Sobrenome:', sobrenome, 'foi Registrado'),
+print('Nascimento: ', ano_nascimento),
+print('Usuario maior de idade?:', maior_idade),
+print('Com altura:', altura_metros),
